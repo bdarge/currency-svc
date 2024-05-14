8b4b7295103e4b9b2c4a841ab2b41f0201b50fea
bdarge <bin.darge@gmail.com>
2024-05-14 17:42:14 -0400

    move proto to submodule

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    tonic_build::compile_protos("proto/currency.proto")
+    tonic_build::compile_protos("pb/currency.proto")
     .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
